2b703da42a4de2b40a8c1043c501877253bd69b0
gaelforget <gforget@mit.edu>
2025-05-30 10:22:35 -0700

    include BudykoSellers, deps, exports

--- src/ECCO.jl
+++ src/ECCO.jl
@@ -6,7 +6,11 @@ import Climatology, MeshArrays, MITgcm, Optim, Enzyme, AirSeaFluxes
 include("initial_examples.jl")
 include("mountain_glacier.jl")
 include("Lorenz_models.jl")
+include("BudykoSellers_model.jl")
 
 Zygote_ex1=Zygote_examples.Zygote_ex1
+
+export Budyko_Sellers_models, Lorenz_models, glacier_model
+export toy_problems, Zygote_examples
 
 end
