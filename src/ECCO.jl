950c58c710d90e0762580b3ee741004007b75f1f
gaelforget <gforget@mit.edu>
2026-06-17 21:54:16 -0400

    fix on 1.10

--- src/ECCO.jl
+++ src/ECCO.jl
@@ -3,14 +3,6 @@ module ECCO
 # Write your package code here.
 import Climatology, MITgcm, Optim, AirSeaFluxes
 
-include("initial_examples.jl")
-include("mountain_glacier.jl")
-include("Lorenz_models.jl")
-include("BudykoSellers_model.jl")
-
-Zygote_ex1=Zygote_examples.Zygote_ex1
-DifferentiationInterface_ex1=DifferentiationInterface_example.DifferentiationInterface_ex1
-
 _autodiff_Reverse() = (@warn "undefined")
 _Reverse() = (@warn "undefined")
 _Active() = (@warn "undefined")
@@ -30,6 +22,14 @@ function dTdt_solve()
     @warn "requires using dTdt_solve"
     error("Placeholder for dTdt_solve should never be used")
 end
+
+include("initial_examples.jl")
+include("mountain_glacier.jl")
+include("Lorenz_models.jl")
+include("BudykoSellers_model.jl")
+
+Zygote_ex1=Zygote_examples.Zygote_ex1
+DifferentiationInterface_ex1=DifferentiationInterface_example.DifferentiationInterface_ex1
 
 export Budyko_Sellers_models, Lorenz_models, glacier_model
 export toy_problems, Zygote_examples
